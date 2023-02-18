0c71fafbe21078f7411f8eb1bfb55107332ad442
Steven Warren <stejwar@amazon.com>
2023-02-18 15:48:26 -0700

    Added for packer to test build and preinstall reqs

---
+++ pre-install-check.py
@@ -0,0 +1,31 @@
+from nataili.model_manager.compvis import CompVisModelManager
+from nataili.stable_diffusion.compvis import CompVis
+
+def runMain():
+    # The model manager loads and unloads the  SD models and has features to download them or find their location
+    mm = CompVisModelManager()
+    # The model to use for the generation].
+    models = ["stable_diffusion"]
+    for model in models:
+        mm.load(model)
+
+        compvis = CompVis(
+            model=mm.loaded_models[model],
+            model_name=model,
+            output_dir="output_dir",
+            disable_voodoo=True,
+            filter_nsfw=False,
+            safety_checker=None,
+        )
+        compvis.generate(
+            prompt="a large cubism cake",
+            sampler_name="k_euler_a",
+            ddim_steps=15,
+            seed=2,
+            init_img=None,
+            sigma_override=None,
+            clip_skip=1
+        )
+
+if __name__ == "__main__":
+    runMain()
